bfa1b9fd1a128471189ea5fc6987a24e9073ba84
Viktor Enbom <116982145+GodtyckligMatematiker@users.noreply.github.com>
2024-07-03 12:36:04 +0200

    Finished implemented working instruction encoder/decoder.

--- assembly.py
+++ assembly.py
@@ -37,21 +37,27 @@ class Assembly:
             self.line_sizes.append(possible_lines)
             self.size += possible_lines
     
-    def instruction_to_num(self, instruction):
+    def instruction_encode(self, instruction):
         #Converts an assembly instruction in the vocabulary to a number
+        #The idea is to have all instructions be iterable from 1 to the number of possible instructions
         self.previous_line_sizes = 0
         for (i, line) in enumerate(self.vocab):
             words_in_line = line.split(" ")
             words_in_instruction = instruction.split(" ")
+
+            if len(words_in_line) != len(words_in_instruction):
+                self.previous_line_sizes += self.line_sizes[i]
+                continue
+                
             current_line_index = 0
             remaining = self.line_sizes[i]
             for (j, word_in_line) in enumerate(words_in_line):
                 word_in_instruction = words_in_instruction[j]
                 if word_in_line == "REG" and word_in_instruction in self.registers:
-                    remaining /= len(self.registers)
+                    remaining //= len(self.registers)
                     current_line_index += remaining * self.registers.index(word_in_instruction)
                 elif word_in_line == "MEM" and word_in_instruction in self.mem_locs:
-                    remaining /= len(self.mem_locs)
+                    remaining //= len(self.mem_locs)
                     current_line_index += remaining * self.mem_locs.index(word_in_instruction)
                 elif word_in_line == word_in_instruction:
                     pass
@@ -63,19 +69,30 @@ class Assembly:
                 
             self.previous_line_sizes += self.line_sizes[i]
     
-    def num_to_instruction(self, num):
+    def instruction_decode(self, num):
+        #Converts a num to an assembly instruction
         cumulative = 0
         line_nr = -1
-        while cumulative < num:
+        while cumulative < num and line_nr < len(self.line_sizes) - 1:
             line_nr += 1
-            cumulative += self.line_sizes[num]
-        remaining = num - self.line_sizes[num]
-        for word in self.vocab[line_nr]:
+            cumulative += self.line_sizes[line_nr]
+
+        words = list(reversed(self.vocab[line_nr].split(" ")))
+        instruction = []
+        for word in words:
             if word == "REG":
-                remaining /= len(self.registers)
+                instruction.append(self.registers[num % len(self.registers)])
+                num = (num - num % len(self.registers)) // len(self.registers)
             elif word == "MEM":
-                remaining /= len(self.mem_locs)
+                instruction.append(self.mem_locs[num % len(self.mem_locs)])
+                num = (num - num % len(self.mem_locs)) // len(self.mem_locs)
+            else:
+                instruction.append(word)
+
+        return " ".join(list(reversed(instruction)))
 
 if __name__ == "__main__":
     a = Assembly()
-    print(a.instruction_to_num("mov %rbx %rax"))
+    num = a.instruction_encode("mov %rbx -0x4(%rbp)")
+    print(num)
+    print(a.instruction_decode(num))

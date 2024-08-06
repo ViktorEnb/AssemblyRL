119578981875edd885b119c7f3613965b27809d2
Viktor Enbon <viktorenbom01@gmail.com>
2024-08-06 16:18:45 +0200

    Removed toy game without representation.

--- game.py
+++ game.py
@@ -24,57 +24,6 @@ class Game:
 
 
 #Class used for testing mcts algorithm
-class ToyGame(Game):
-    def __init__(self):
-        self.tree = {
-            0: [1, 2],      
-            1: [3, 4],
-            2: [5, 6],
-            3: [],
-            4: [],
-            5: [],
-            6: []
-        }
-        self.rewards = {
-            3: 10,
-            4: 5,
-            5: -3,
-            6: 2
-        }
-        #Boolean value indicating whether the game uses a neural network to represent states
-        self.has_repr_network = False
-    #Returns the unique states (not represantations) of the game
-    def get_unique_states(self):
-        return self.tree.keys()
-    def initialize_state(self):
-        # Start from the root node
-        return torch.tensor([0],dtype=torch.float32)
-
-    def get_reward(self, node):
-        if self.is_terminal(node):
-            num = node.state[0].item()
-            return self.rewards[num]
-        return 0
-
-    def get_actions(self, node):
-        return range(0,2)
-    def get_num_actions(self, state):
-        return 2
-
-    def is_terminal(self, node):
-        # A state is terminal if it has no children
-        num = node.state[0].item()
-        return len(self.tree[num]) == 0
-
-    def apply_action(self, state, action):
-        # Move to the selected child node
-        num = state[0].item()
-        return torch.tensor([self.tree[num][action]],dtype=torch.float32)
-    def write_game(self, actions, filename="", meta_data=[]):
-        pass
-
-
-#Class used for testing mcts algorithm
 class ToyGameWithReprNetwork(Game):
     def __init__(self):
         self.tree = {
